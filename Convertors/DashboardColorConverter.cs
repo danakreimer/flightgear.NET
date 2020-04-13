173261cc5e01c7479ba788cf98e8d97523dbb2b3
danak1997 <danathekraimer@gmail.com>
2020-04-13 18:54:07 +0300

    removed the not implemented throw

--- Convertors/DashboardColorConverter.cs
+++ Convertors/DashboardColorConverter.cs
@@ -22,7 +22,7 @@ namespace FlightgearSimulator.Convertors
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return value.ToString();
         }
     }
 }
